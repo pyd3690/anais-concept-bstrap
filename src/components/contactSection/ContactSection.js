3517965a282e06397ea9321f1bdebe07e341001c
pyd3690 <prenaamd@gmail.com>
2021-06-18 12:25:03 -0500

    refined hero section

--- src/components/contactSection/ContactSection.js
+++ src/components/contactSection/ContactSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import {InputGroup, Dropdown, Form, Button, FormControl, DropdownButton} from 'react-bootstrap'
+import {InputGroup, Dropdown, Form, Button, FormControl, DropdownButton, ListGroup} from 'react-bootstrap'
 import styles from './ContactSection.module.css'
 import ReactMarkdown from "react-markdown";
 
@@ -37,7 +37,7 @@ const ContactSection = (props) => {
             <InputGroup className="mb-3">
                 <DropdownButton
                 as={InputGroup.Prepend}
-                variant="outline-secondary"
+                variant="warning"
                 title="Comment Vous Assister "
                 id="input-group-dropdown-1"
                 >
@@ -61,10 +61,85 @@ const ContactSection = (props) => {
                 <Form.Label>Message</Form.Label>
                 <Form.Control as="textarea" rows={3} />
             </Form.Group>
-            <Button variant="warning" type="submit">
+            <Button variant="warning" type="submit" style={{marginBottom:'20px'}}>
                 Envoyer
             </Button>
             <br /><br />
+            <h2 className={styles.sectionTitle}> Autre Contact</h2>
+            <ListGroup>
+                <ListGroup.Item className={styles.listItem}>
+                    <div style={{}}>
+                        <img className={styles.iconC}
+                            src="/icons/company.png"
+                            width="auto"
+                            height="20px"
+                            className="d-inline-block align-left"
+                            alt="Facebook Profile"                            
+                        /> 
+                    </div>
+                    <Link href="#"><div className={styles.listItemContent}>Anais Concept</div></Link>
+                </ListGroup.Item>
+                <ListGroup.Item className={styles.listItem}>
+                    <div style={{}}>
+                        <img className={styles.iconC}
+                            src="/icons/address.png"
+                            width="auto"
+                            height="20px"
+                            className="d-inline-block align-left"
+                            alt="Facebook Profile"                            
+                        /> 
+                    </div>
+                    <Link href="#"><div className={styles.listItemContent}>01 BP 2930 Lomé, Lomé</div></Link>
+                </ListGroup.Item>
+                <ListGroup.Item className={styles.listItem}>
+                    <div style={{}}>
+                        <img className={styles.iconC}
+                            src="/icons/phone.png"
+                            width="auto"
+                            height="20px"
+                            className="d-inline-block align-left"
+                            alt="Facebook Profile"                            
+                        /> 
+                    </div>
+                    <Link href="#"><div className={styles.listItemContent}>+228 90 12 31 48</div></Link>
+                </ListGroup.Item>
+                <ListGroup.Item className={styles.listItem}>
+                    <div style={{}}>
+                        <img className={styles.iconC}
+                            src="/icons/whatsapp.png"
+                            width="auto"
+                            height="20px"
+                            className="d-inline-block align-left"
+                            alt="Facebook Profile"                            
+                        /> 
+                    </div>
+                    <Link href="#whatsapp"><div className={styles.listItemContent}>+228 90 12 31 48</div></Link>
+                </ListGroup.Item>
+                <ListGroup.Item className={styles.listItem}>
+                    <div style={{}}>
+                        <img className={styles.iconC}
+                            src="/icons/facebook.png"
+                            width="auto"
+                            height="20px"
+                            className="d-inline-block align-left"
+                            alt="Facebook Profile"                        
+                        /> 
+                    </div>
+                    <Link href="https://www.facebook.com/anaisconceptbiz/" target="_blank"><div className={styles.listItemContent}>https://www.facebook.com/anaisconceptbiz/</div></Link>
+                </ListGroup.Item>
+                <ListGroup.Item className={styles.listItem}>
+                    <div style={{}}>
+                        <img className={styles.iconC}
+                            src="/icons/email.png"
+                            width="auto"
+                            height="20px"
+                            className="d-inline-block align-left"
+                            alt="Facebook Profile"                            
+                        /> 
+                    </div>
+                    <Link href="#"><div className={styles.listItemContent}>infos@anaisconcept.biz</div></Link>
+                </ListGroup.Item>
+            </ListGroup>
         </div>
     )
 }
